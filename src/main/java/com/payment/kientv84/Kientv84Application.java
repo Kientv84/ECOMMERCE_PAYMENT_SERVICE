28698d7fb15c89539dc1fca0d1ace2d03e2768bc
Truong Chi Kien <kientv@MacBook-Pro-cua-Truong.local>
2025-10-31 11:35:59 +0700

    init project

---
+++ src/main/java/com/payment/kientv84/Kientv84Application.java
@@ -0,0 +1,13 @@
+package com.payment.kientv84;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kientv84Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kientv84Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/kientv84/Kientv84ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.kientv84;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kientv84ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
